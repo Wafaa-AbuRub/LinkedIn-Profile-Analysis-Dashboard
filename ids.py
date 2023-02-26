2286213ecbf8f560d372198b0d9f976eae6296d8
Wafa'a Aburub <wafaamabualrub@gmail.com>
2023-02-26 15:58:07 +0300

    Creates an ID reference file.

---
+++ ids.py
@@ -0,0 +1,13 @@
+"""
+{
+    "title": "Dashboard components IDs",
+    "owner": ("Wafa'a Aburub", "wafaa.github@gmail.com"),
+    "description": "Combines IDs of all dashboard-created components."
+}
+"""
+
+
+date_picker_start = "date_picker1_start_id"
+date_picker_end = "date_picker2_end_id"
+
+
